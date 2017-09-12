07da937584667f9c65128466f6ff38d4d1411bf7
user <user@localhost>
2017-09-12 15:44:39 +0530

    Code Warnings Removed

--- src/reducers/patientRegistration-reducer.js
+++ src/reducers/patientRegistration-reducer.js
@@ -6,46 +6,26 @@ export default function reducer(state={
     error: null,
 },action){
     switch(action.type){
-        case "FETCH_PATIENT": {
-            return {state, fetching: true}
-            break;
-        }
-        case "FETCH_PATIENT_REJECTED": {
-            return {state, fetching: false,  error: action.payload}
-            break;
-        }
+        case "FETCH_PATIENT":
+            return {state, fetching: true};
+        case "FETCH_PATIENT_REJECTED":
+            return {state, fetching: false,  error: action.payload};
         case "FETCH_PATIENT_FULFILLED":
-        {
-            return {state, fetching: false, fetched: true, patients: action.payload}
-            break;
-        }
-        case "SAVE_PATIENT_REJECTED": {
-            return {state, fetching: false,  error: action.payload}
-            break;
-        }
+            return {state, fetching: false, fetched: true, patients: action.payload};
+        case "SAVE_PATIENT_REJECTED":
+            return {state, fetching: false,  error: action.payload};
         case "SAVE_PATIENT_FULFILLED":
-        {
-            return {state, fetching: false, fetched: true, patients: action.payload}
-            break;
-        }
-        case "UPDATE_PATIENT_REJECTED": {
-            return {state, fetching: false,  error: action.payload}
-            break;
-        }
+            return {state, fetching: false, fetched: true, patients: action.payload};
+        case "UPDATE_PATIENT_REJECTED":
+            return {state, fetching: false,  error: action.payload};
         case "UPDATE_PATIENT_FULFILLED":
-        {
-            return {state, fetching: false, fetched: true, patients: action.payload}
-            break;
-        }
-        case "DELETE_PATIENT_REJECTED": {
-            return {state, fetching: false,  error: action.payload}
-            break;
-        }
+            return {state, fetching: false, fetched: true, patients: action.payload};
+        case "DELETE_PATIENT_REJECTED":
+            return {state, fetching: false,  error: action.payload};
         case "DELETE_PATIENT_FULFILLED":
-        {
-            return {state, fetching: false, fetched: true, patients: action.payload}
-            break;
-        }
+            return {state, fetching: false, fetched: true, patients: action.payload};
+
+        default: break;
 
     }
 
